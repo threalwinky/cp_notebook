6583b478b8cc4df504ef6d9353aa707ca963d23d
threalwinky <voquangvu09112006@gmail.com>
2023-09-01 02:27:47 +0700

    upload files

---
+++ winky/template.cpp
@@ -0,0 +1,65 @@
+#include                    <bits/stdc++.h>
+using                       namespace std;
+#define fi                  first
+#define se                  second
+#define endl                cout<<'\n';
+//Data types macros
+//#define int                 long long //uncomment if needed
+using ll =                  long long;
+using ull =                 unsigned long long;
+using ldb =                 long double; //or double if TL is tight
+using str =                 string; //python type
+using ii =                  pair<int, int>;
+using mii =                 map<int, int>;
+#define mp                  make_pair
+#define tcT                 template<class T
+#define tcTU                tcT, class U
+tcT > using V =             vector<T>;
+using vi =                  V<int>;
+using vb =                  V<bool>;
+using vll =                 V<ll>;
+using vull =                V<ull>;
+using vldb =                V<ldb>;
+using vii =                 V<ii>;
+using vstr =                V<str>;
+//Vector functions
+#define sz(x)               (int)(x.size())
+#define bg(x)               begin(x)
+#define all(x)              begin(x), end(x)
+#define ft                  front
+#define bk                  back
+#define pb                  push_back
+#define lb                  lower_bound
+#define ub                  upper_bound
+//Loops
+#define FOR(i,a,b)          for (int i=(a); i<(b); i++)
+#define FORD(i,a,b)         for (int i=(a)-1; i>=(b); i--)
+#define F0R(i,a)            FOR(i,0,a)
+#define F0RD(i,a)           FORD(i,0,a)
+#define FORE(i,a)           for (auto i:a)
+//File accessing
+const string FI =           "sample";
+const string IFI =          FI + ".inp";
+const string OFI =          FI + ".out";
+void opf(bool c){           if (c == 1){
+freopen(IFI.c_str(),        "r", stdin);
+freopen(OFI.c_str(),        "w", stdout);}}
+//Constants
+const ldb PI =              3.14159265358979;
+const int maxI =            INT_MAX;
+const int maxll =           LLONG_MAX;
+const int N =               1e6;
+const int mod  =            1e9 + 7;
+mt19937 rng((uint32_t)chrono::steady_clock::now().time_since_epoch().count());
+//------------------------------------------------------------------------------------
+//Code here
+
+
+
+//------------------------------------------------------------------------------------
+signed main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    opf(0);
+
+}
